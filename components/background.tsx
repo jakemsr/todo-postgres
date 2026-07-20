c90f28eb7038cce6b26f5fea640085667640a71f
jakemsr <jakemsr@yahoo.com>
2026-07-20 15:29:56 -0700

    fix: Use mobile friendly screen height

--- components/background.tsx
+++ components/background.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "@/components/theme-toggle";
 
 export default function Background({children}: {children: React.ReactNode}) {
   return (
-    <main role="main" className="min-h-screen flex justify-center bg-black">
+    <main role="main" className="min-h-svh flex justify-center bg-black">
       <div className="w-360 bg-white dark:bg-navy-950 bg-hero bg-top bg-no-repeat flex flex-col items-center">
         <div className="flex justify-between w-140 mt-20 text-white font-bold text-5xl tracking-[0.3em] uppercase">
           todo
