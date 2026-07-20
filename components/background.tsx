bb159ab0052112b5c65345a66deede706179ddda
jakemsr <jakemsr@yahoo.com>
2026-07-20 15:30:34 -0700

    fix: Same width as todo content

--- components/background.tsx
+++ components/background.tsx
@@ -5,7 +5,7 @@ export default function Background({children}: {children: React.ReactNode}) {
   return (
     <main role="main" className="min-h-svh flex justify-center bg-black">
       <div className="w-360 bg-white dark:bg-navy-950 bg-hero bg-top bg-no-repeat flex flex-col items-center">
-        <div className="flex justify-between w-140 mt-20 text-white font-bold text-5xl tracking-[0.3em] uppercase">
+        <div className="flex justify-between w-136 mt-20 text-white font-bold text-5xl tracking-[0.3em] uppercase">
           todo
           <ThemeToggle />
         </div>
